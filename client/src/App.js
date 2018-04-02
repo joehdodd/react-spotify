26f0eba217c6a19facd74aa536cc103cb48cb42e
Joe Dodd <joehdodd@gmail.com>
2018-04-02 17:07:49 -0400

    app structure

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <a href='http://localhost:8888/login'>Login to Spotify</a>
+      </div>
+    );
+  }
+}
